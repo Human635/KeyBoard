308ddbc23c457188b073c013c0142cb6243974e9
Xojiakbar <139034828+Human635@users.noreply.github.com>
2023-08-03 15:37:31 +0500

    C|L & Shift("+" - "-");  Space & Backspace(good)

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const shiftButton = document.getElementById('Shift');
+const textInput = document.getElementById('text-input');
+
 let keys = document.querySelectorAll('.key');
 
 function keyPress(event) {
@@ -8,11 +11,9 @@ function keyPress(event) {
       let textInput = document.querySelector('#text-input');
       textInput.value =  textInput.value.slice(0, -1);
     break;
-    case 'caps lock':
-      let keyCL = document.querySelector('#text-input');
-      let uppercase = key.classList.toggle('active');
-      let text = keyCL.value;
-      keyCL.value = uppercase ? text.toUpperCase() : text.toLowerCase();
+    case 'space':
+      let textInputS = document.querySelector('#text-input');
+      textInputS.value += ' ';
     break;
     default:
       let textInputt = document.querySelector('#text-input');
@@ -24,4 +25,33 @@ function keyPress(event) {
 
 keys.forEach(key => {
   key.addEventListener('click', keyPress);
+});
+
+function toggleCapsLock() {
+  const textInput = document.getElementById('text-input');
+
+  if (textInput.style.textTransform === "uppercase") {
+    textInput.style.textTransform = "none";
+  } else {
+    textInput.style.textTransform = "uppercase";
+  }
+}
+
+let clickCount = 0;
+
+shiftButton.addEventListener('click', function() {
+    clickCount++;
+    const currentValue = textInput.value;
+    const lastChar = currentValue.slice(-1);
+    let newValue;
+    
+    if (clickCount === 2) {
+        newValue = currentValue.slice(0, -1) + lastChar.toLowerCase();
+    } else {
+        newValue = currentValue.slice(0, -1) + lastChar.toUpperCase();
+    }
+    
+    textInput.value = newValue;
+    
+    clickCount = clickCount >= 2 ? 0 : clickCount;
 });
